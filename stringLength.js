cb24160b2fabe4859a8e93081e77fbee19614df3
Diego Luna <slowkingvii@gmail.com>
2022-07-21 00:57:37 -0500

    Expand 'stringLength()' to check for string boundaries

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,10 @@
 const stringLength = (string) => {
-  return string.length;
+  const length = string.length;
+
+  if (length < 1) throw new Error('String cannot be empty!');
+  if (length > 10) throw new Error('String cannot be more than 10 characters long');
+
+  return length;
 }
 
 module.exports = stringLength;
